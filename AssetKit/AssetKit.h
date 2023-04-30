189c0c02270a54e9b58b1e52e591cb9e7ffca836
kelvinfok <punkd1@gmail.com>
2023-04-30 16:37:07 +0800

    initial commit

---
+++ AssetKit/AssetKit.h
@@ -0,0 +1,18 @@
+//
+//  AssetKit.h
+//  AssetKit
+//
+//  Created by Kelvin Fok on 29/4/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AssetKit.
+FOUNDATION_EXPORT double AssetKitVersionNumber;
+
+//! Project version string for AssetKit.
+FOUNDATION_EXPORT const unsigned char AssetKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AssetKit/PublicHeader.h>
+
+
--- SomeDemoApp/ViewController.swift
+++ SomeDemoApp/ViewController.swift
@@ -6,14 +6,28 @@
 //
 
 import UIKit
+import AssetKit
 
 class ViewController: UIViewController {
 
+  private let imageView: UIImageView = {
+    let imageView = UIImageView()
+    imageView.backgroundColor = .blue
+    imageView.translatesAutoresizingMaskIntoConstraints = false
+    return imageView
+  }()
+
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
-  }
 
+    view.addSubview(imageView)
+    NSLayoutConstraint.activate([
+      imageView.heightAnchor.constraint(equalToConstant: 200),
+      imageView.widthAnchor.constraint(equalToConstant: 200),
+      imageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+      imageView.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+    ])
+  }
 
 }
 
